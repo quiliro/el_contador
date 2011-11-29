ba519af4df7c214cc66ecda0d16a44cfc31c121f
Quiliro Ordóñez <quiliro@congresolibre.org>
2011-11-29 18:23:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElContador::Application.config.secret_token = '0525bb233208b318f5390666df8f784e09c02d1dd0054e91478a18657923d8d327167d9fbe8f864693dd92e9530fa7bacc223935f20df33a8bdafc64d1d16722'
